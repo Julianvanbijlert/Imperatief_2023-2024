48cd7116a5963ec698e28314f5c363ee8d5c5454
Julianvanbijlert <116498773+Julianvanbijlert@users.noreply.github.com>
2023-09-18 21:10:37 +0200

    emmas mandelbrot gemaakt en die werkt nu

---
+++ Nieuwe projecten/Emma's Mandelbrot/Program.cs
@@ -0,0 +1,82 @@
+            using System;
+            using System.Data;
+            using System.Data.Common;
+            using System.Drawing;
+            using System.Windows.Forms;
+namespace Emma_s_Mandelbrot
+{
+    internal static class Program
+    {
+
+        static void Main()
+        {
+
+
+            //Globale variabelen
+
+            int algemene_grootte = 100;
+            int breedte_scherm = 400;
+            int hoogte_scherm = 400;
+
+
+
+
+
+            //-----------------------------------------------------Schermtekenen---------------------------------------------------
+
+            //de form
+            Form scherm = new Form();
+            scherm.Text = "mandelbrot windows";
+            scherm.ClientSize = new Size(breedte_scherm, hoogte_scherm);
+
+
+            //bitmap voor 
+            Bitmap plaatje = new Bitmap(breedte_scherm, hoogte_scherm);
+            Graphics gr = Graphics.FromImage(plaatje);
+            Label afbeelding = new Label();
+
+
+
+
+
+
+
+
+            //-----------------------------------------------------Mandelbrot Tekenen----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+            //-----------------------------------------------------Knoppen en events----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Application.Run(scherm);
+        }
+    }
+}

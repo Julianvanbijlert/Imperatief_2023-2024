12eed6efc38fbba2a60354b683088362d701bde9
Julianvanbijlert <116498773+Julianvanbijlert@users.noreply.github.com>
2023-09-21 11:26:00 +0200

    functies voor buttons en labels maken aangemaakt

--- Nieuwe projecten/Emma's Mandelbrot/Program.cs
+++ Nieuwe projecten/Emma's Mandelbrot/Program.cs
@@ -2,7 +2,9 @@
             using System.Data;
             using System.Data.Common;
             using System.Drawing;
-            using System.Windows.Forms;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
 namespace Emma_s_Mandelbrot
 {
     internal static class Program
@@ -56,11 +58,40 @@ namespace Emma_s_Mandelbrot
 
 
             //-----------------------------------------------------Knoppen en events----------------------------------------------------------
+            //we zijn van plan meerdere labels te maken met veel dezelfde attributen dus dan hebben wij een functie gemaakt om deze aan te maken.
+            Label MakeLabel(int x, int y, string s)
+            {
+                Label L   = new Label();
+                L.Size = new Size(80, 20);
+                L.Location = new Point(x, y);
+                L.BackColor = Color.Aquamarine ;
+                scherm.Controls.Add(L);
+                L.Text = s;
+                return L;
+            }
 
+            //we zijn van plan meerdere buttons te maken met veel dezelfde attributen dus dan hebben wij een functie gemaakt om deze aan te maken.
+            Button MakeButton(int x, int y, string s)
+            {
+                Button b = new Button();
+                b.Size = new Size(80, 20);
+                b.Location = new Point(x, y);
+                b.BackColor = Color.AliceBlue;
+                b.Text = s;
 
+                b.MouseMove += (sender, e) =>
+                {
+                    b.BackColor = Color.LightGray;
+                };
+                b.MouseLeave += (sender, e) => {
+                    b.BackColor= Color.AliceBlue;
+                };
+                scherm.Controls.Add(b);
+                return b;
+            }
 
-
-
+            Button b = MakeButton(120, 50,"start");
+            Label label = MakeLabel( 30, 50, "Wie dit leest is gek");
 
 
 
